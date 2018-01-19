4f5dbd66a72bc6c2c0f8444ca4b22a4cc584957b
Philippe <philippe@ppmt.org>
2018-01-19 15:27:37 +0000

    add new crimes functionality

--- dbhelper.py
+++ dbhelper.py
@@ -1,12 +1,13 @@
+import datetime
 import pymysql
 import dbconfig
 
 class DBHelper:
 	def connect(self, database="crimemap"):
-            return pymysql.connect(host='localhost',
-							   user=dbconfig.db_user,
-							   passwd=dbconfig.db_password,
-							   db=database)
+		return pymysql.connect( host='localhost',
+								user=dbconfig.db_user,
+								passwd=dbconfig.db_password,
+								db=database)
 
 	def get_all_inputs(self):
             connection = self.connect()
@@ -17,6 +18,26 @@ class DBHelper:
                 return cursor.fetchall()
             finally:
                 connection.close()
+
+	def get_all_crimes(self):
+		connection = self.connect()
+		try:
+			query = "SELECT latitude, longitude, date, category, description FROM crimes;"
+			with connection.cursor() as cursor:
+				cursor.execute(query)
+			named_crimes = []
+			for crime in cursor:
+				named_crime = {
+								'latitude': crime[0],
+								'longitude': crime[1],
+								'date': datetime.datetime.strftime(crime[2], '%Y-%m-%d'),
+								'category': crime[3],
+								'description': crime[4]
+								}
+				named_crimes.append(named_crime)
+			return named_crimes
+		finally:
+			connection.close()
 
 	def add_input(self, data):
 		connection = self.connect()
@@ -38,16 +59,14 @@ class DBHelper:
 		finally:
 			connection.close()
 
-        def add_crime(self, category, date, latitude, longitude,
-                description):
+        def add_crime(self, category, date, latitude, longitude, description):
             connection = self.connect()
             try:
                 query = "INSERT INTO crimes (category, date, latitude, longitude, description) \
                         VALUES (%s, %s, %s, %s, %s)"
-                        with connection.cursor() as cursor:
-                            cursor.execute(query, (category, date, latitude, longitude,
-                                description))
-                            connection.commit()
+                with connection.cursor() as cursor:
+			cursor.execute(query, (category, date, latitude, longitude, description))
+			connection.commit()
             except Exception as e:
                 print(e)
             finally:
--- mockdbhelper.py
+++ mockdbhelper.py
@@ -3,6 +3,12 @@ class MockDBHelper:
         pass
     def get_all_inputs(self):
         return []
+    def get_all_crimes(self):
+	return [{ 'latitude': -52.912289,
+		  'longitude': 2.523355,
+		  'date': "2000-01-01",
+		  'category': "mugging",
+		  'description': "mock description" }]
     def add_input(self, data):
         pass
     def clear_all(self):
