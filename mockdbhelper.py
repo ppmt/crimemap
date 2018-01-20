fbc7f2c9cc9e7585817ef4fb1c5872f7237bc667
Philippe <philippe@ppmt.org>
2018-01-20 13:26:56 +0000

    fix mock postion

--- mockdbhelper.py
+++ mockdbhelper.py
@@ -4,8 +4,8 @@ class MockDBHelper:
     def get_all_inputs(self):
         return []
     def get_all_crimes(self):
-	return [{ 'latitude': -52.912289,
-		  'longitude': 2.523355,
+	return [{ 'latitude': 52.912289,
+		  'longitude': -2.523355,
 		  'date': "2000-01-01",
 		  'category': "mugging",
 		  'description': "mock description" }]
