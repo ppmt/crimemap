2de0f14ca569c75cf118bb4070fc39112c6f01af
Philippe <philippe@ppmt.org>
2018-01-21 12:32:24 +0000

    data sanitized against MOST hacking

--- dbhelper.py
+++ dbhelper.py
@@ -5,19 +5,9 @@ import dbconfig
 class DBHelper:
 	def connect(self, database="crimemap"):
 		return pymysql.connect( host='localhost',
-								user=dbconfig.db_user,
-								passwd=dbconfig.db_password,
-								db=database)
-
-	def get_all_inputs(self):
-            connection = self.connect()
-            try:
-                query = "SELECT description FROM crimes;"
-                with connection.cursor() as cursor:
-                    cursor.execute(query)
-                return cursor.fetchall()
-            finally:
-                connection.close()
+					user=dbconfig.db_user,
+					passwd=dbconfig.db_password,
+					db=database)
 
 	def get_all_crimes(self):
 		connection = self.connect()
@@ -28,34 +18,14 @@ class DBHelper:
 			named_crimes = []
 			for crime in cursor:
 				named_crime = {
-								'latitude': crime[0],
-								'longitude': crime[1],
-								'date': datetime.datetime.strftime(crime[2], '%Y-%m-%d'),
-								'category': crime[3],
-								'description': crime[4]
-								}
+						'latitude': crime[0],
+						'longitude': crime[1],
+						'date': datetime.datetime.strftime(crime[2], '%Y-%m-%d'),
+						'category': crime[3],
+						'description': crime[4]
+						}
 				named_crimes.append(named_crime)
 			return named_crimes
-		finally:
-			connection.close()
-
-	def add_input(self, data):
-		connection = self.connect()
-		try:
-			query = "INSERT INTO crimes (description) VALUES (%s);"
-			with connection.cursor() as cursor:
-				cursor.execute(query,data)
-				connection.commit()
-		finally:
-			connection.close()
-
-	def clear_all(self):
-		connection = self.connect()
-		try:
-			query = "DELETE FROM crimes;"
-			with connection.cursor() as cursor:
-				cursor.execute(query)
-				connection.commit()
 		finally:
 			connection.close()
 
--- mockdbhelper.py
+++ mockdbhelper.py
@@ -1,18 +1,12 @@
 class MockDBHelper:
     def connect(self, database="crimemap"):
         pass
-    def get_all_inputs(self):
-        return []
     def get_all_crimes(self):
 	return [{ 'latitude': 52.912289,
 		  'longitude': -2.523355,
 		  'date': "2000-01-01",
 		  'category': "mugging",
 		  'description': "mock description" }]
-    def add_input(self, data):
-        pass
-    def clear_all(self):
-        pass
     def add_crime(self, category, date, latitude, longitude,
             description):
         pass
